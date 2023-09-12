ff60bdd0955922acb07d355fdb64ce9a835991f3
Mahmoud amr <113695259+mahmod5000@users.noreply.github.com>
2023-09-12 18:47:14 +0300

    hlaf of project

--- main.cpp
+++ main.cpp
@@ -1,123 +1,25 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+const char ESC = 27;
+#define ll long long
 #define Endl endl
-//sec6
-class cstring
+class Student
 {
 private:
-    string x;
+    int Id ; string first_name , last_name;
 public:
-    cstring():x(""){};
-    cstring(string x){this->x=x;}
-    cstring operator+(cstring ob)
-    {
-        x+=" "+ob.x;
-        return *this;
-    }
-    friend ostream&operator<<(ostream&cout,cstring ob)
-    {
-        cout<<ob.x;
-        return cout;
-    }
-    friend istream&operator>>(istream&cin,cstring &ob);
-    bool operator==(cstring ob)
-    {
-        return (x==ob.x);
-    }
+    Student():Id(0),first_name("no name"),last_name("no name"){};
+    void SetId(int Id);
+    void SetFirstName(string first_name);
 };
-istream&operator>>(istream&cin,cstring &ob)
+void Student ::SetId(int Id)
 {
-    cin>>ob.x;
-    return cin;
+    this-> Id = Id;
 }
-class binary{
-private:
-    float x;
-public:
-    binary():x(0.0){};
-    binary(float y):x(y){};
-    binary operator +(binary ob);
-    binary operator -(binary ob);
-    binary operator /(binary ob);
-    binary operator++();
-    binary operator++(int);
-    binary operator +=(binary ob)
-    {
-        x+=ob.x;
-        return *this;
-    }
-    friend ostream&operator<<(ostream&cout,binary ob)
-    {
-        cout<<ob.x;return cout;
-    }
-    friend istream&operator>>(istream &cin,binary &ob);
-};
-binary binary:: operator+(binary ob) {
-    x+=ob.x;
-    return *this;
-}
-binary binary::operator-(binary ob) {
-    x-=ob.x;
-    return *this;
-}
-binary binary::operator/(binary ob) {
-    x/=ob.x;
-    return *this;
-}
-istream&operator>>(istream &cin,binary &ob)
+void Student ::SetFirstName(string  first_name)
 {
-    cin>>ob.x;
-    return cin;
+    this -> first_name =first_name;
 }
-binary binary:: operator++()
-{
-    x++;
-    return x;
-}
-binary binary ::operator++(int) {
-    binary temp;
-    temp.x=x;
-    x++;
-    return temp;
-}
-class rec;
-class square{
-private:
-    int l,area;
-public:
-    square(int x=0):l(x){};
-    int Get_arae(){return l*l;}
-    friend rec;
-};
-class rec{
-private:
-    int l,w;
-public:
-    rec(int x=0,int  y=0):l(x),w(y){};
-    int get_area(){return l*w;}
-    friend int operator +(square s,rec r);
-    friend void largest (square s, rec r);
-};
-int operator +(square s,rec r)
-{
-    int add_area;
-    add_area=s.Get_arae()+r.get_area();
-    return add_area;
-}
-void largest (square s, rec r)
-{
-    if(s.Get_arae()>r.get_area())
-    {
-        cout<<s.Get_arae()<<endl;
-    }
-    else
-    {
-        cout<<r.get_area()<<endl;
-    }
-}
-int main()
-{
-   square ob1(5);rec ob2(7,7);
-   cout<<ob1+ob2<<endl;
-    largest(ob1,ob2);
+int main () {
+
 }
